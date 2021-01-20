32aa4cde99a6b5cbf69827d39c7bc78ccef63702
Andrii Khaulin <60609219+akhaulin@users.noreply.github.com>
2021-01-20 20:08:39 +0200

    HEH-UI-4: Added card discount component

--- heh-ui/src/app/modules/app-material/app-material.module.ts
+++ heh-ui/src/app/modules/app-material/app-material.module.ts
@@ -1,13 +1,15 @@
-import {NgModule} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatInputModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatChipsModule,
   ],
   imports: [],
   providers: []
