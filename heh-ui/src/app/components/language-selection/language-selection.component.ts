16a8d8ef1c510a251f1493c14bdff8e16464ceea
IvanKryvenko <ivank@DESKTOP-IODRHRH>
2021-01-20 15:57:35 +0200

    HEH-UI-8: Create language selection component

---
+++ heh-ui/src/app/components/language-selection/language-selection.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-language-selection',
+  templateUrl: './language-selection.component.html',
+  styleUrls: ['./language-selection.component.scss']
+})
+export class LanguageSelectionComponent {
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+}
--- heh-ui/src/app/modules/app-material/app-material.module.ts
+++ heh-ui/src/app/modules/app-material/app-material.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatInputModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSelectModule,
   ],
   imports: [],
   providers: []
