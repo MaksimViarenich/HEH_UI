088a8e059659e5d74f93b3db080bf4acf73d2bfb
Andrii Khaulin <60609219+akhaulin@users.noreply.github.com>
2021-01-23 13:58:40 +0200

    HEH-UI-11: Modified favorite card component, removed redundant lines from ts, html and scss files

--- heh-ui/src/app/components/favorite-card/favorite-card.component.ts
+++ heh-ui/src/app/components/favorite-card/favorite-card.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-favorite-card',
   templateUrl: './favorite-card.component.html',
-  styleUrls: ['./favorite-card.component.scss']
+  styleUrls: ['./favorite-card.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class FavoriteCardComponent implements OnInit {
 
   bgVendor: string = '../../../assets/images/card-backgrounds/bg-vendor-green.PNG';
   discountVendorName: string = 'Domino\'s pizza';
   discountName: string = 'Buy our tasty pizza with 10% discount loafsdfasdf asdfasdfa sdfas fasdf asdfasdf asdfa sdfasdfasdf asdf asd f';
-  discountNote: string = 'Note: Pepperoni pizza  is tastier with double cheese jfjs  fjsfjs skdfjksdjf ks skdfjklsd ljsdfj lsdjf l sljsdflj lsdfljjsd lsdfkljsfj ljlsdjflsj asdfasdfasdfa sdf asdfas fasdfasdf asfasfasdfadf asdf asfas fasfasdfasd fasf a fas asdf fl';
+  discountNote: string = 'Pepperoni pizza  is tastier with double cheese';
   discountCategory: string = 'Food';
 
   //Mught be an array
@@ -20,5 +21,4 @@ export class FavoriteCardComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
