045ccecdff9c2d81cfd30a2234d754204f14ade8
Andrii Khaulin <60609219+akhaulin@users.noreply.github.com>
2021-01-26 18:00:52 +0200

    HEH-UI-22: Rendered discount cards, faced issues with models

--- heh-ui/src/app/components/discount-card/discount-card.component.ts
+++ heh-ui/src/app/components/discount-card/discount-card.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Discount} from '../../models/discount';
+import { Component, OnInit, Input } from '@angular/core';
+import { DiscountCard } from '../../models/discount-card';
 
 @Component({
   selector: 'app-discount-card',
@@ -7,26 +7,8 @@ import {Discount} from '../../models/discount';
   styleUrls: ['./discount-card.component.scss']
 })
 export class DiscountCardComponent implements OnInit {
-  discount: Discount = {
-    vendor: 'Domino\'s Pizza',
-    title: 'Buy our tasty pizza with 10% discount',
-    category: 'Food',
-    tags: ['Pizza'],
-    description: 'Discount available 24/7 for all our pizzas. 10% for weekdays 15% for weekends',
-    addressList: ['Belarus, Minsk, Komsomolskaya street, 3',
-      'Belarus, Minsk, Lenina street, 25',
-      'Belarus, Minsk, Sverdlova street, 1a',
-      'Belarus, Minsk, Skryganova street, 48',
-      'Belarus, Minsk, Molodezhnaya street, 11',
-      'Belarus, Minsk, Esenina street, 5'],
-    website: 'https://www.dominos.by',
-    phones: ['7717', '(029) 750-37-16', '(044) 750-37-16'],
-    workingHours: '06:00 - 23:00',
-    validity: new Date(2021, 11, 31),
-    instagram: 'https://www.instagram.com/',
-    facebook: 'https://www.facebook.com/',
-    vk: 'https://vk.com/',
-  };
+
+  @Input() discountInfo: DiscountCard | undefined;
 
   constructor() { }
 
--- heh-ui/src/app/pages/discounts/discounts.component.ts
+++ heh-ui/src/app/pages/discounts/discounts.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { DiscountDetailsModalComponent } from './discount-details-modal/discount-details-modal.component';
+import { DiscountCard } from '../../models/discount-card';
 
 @Component({
   selector: 'app-discounts',
@@ -9,10 +10,82 @@ import { DiscountDetailsModalComponent } from './discount-details-modal/discount
 })
 export class DiscountsComponent implements OnInit {
 
+  discounts: Array<DiscountCard> = [
+    {
+      background: '../../../assets/images/card-backgrounds/bg-vendor-green.png',
+      discount: {
+        vendor: 'Domino\'s Pizza TEST',
+        title: 'Buy our tasty pizza with 10% discount',
+        category: 'Food',
+        tags: ['Pizza', ],
+        description: 'Discount available 24/7 for all our pizzas. 10% for weekdays 15% for weekends',
+        addressList: ['Belarus, Minsk, Komsomolskaya street, 3',
+          'Belarus, Minsk, Lenina street, 25',
+          'Belarus, Minsk, Sverdlova street, 1a',
+          'Belarus, Minsk, Skryganova street, 48',
+          'Belarus, Minsk, Molodezhnaya street, 11',
+          'Belarus, Minsk, Esenina street, 5'],
+        website: 'https://www.dominos.by',
+        phones: ['7717', '(029) 750-37-16', '(044) 750-37-16'],
+        workingHours: '06:00 - 23:00',
+        validity: new Date(2021, 11, 31),
+        instagram: 'https://www.instagram.com/',
+        facebook: 'https://www.facebook.com/',
+        vk: 'https://vk.com/',
+      }
+    },
+    {
+      background: '../../../assets/images/card-backgrounds/bg-vendor-blue.png',
+      discount: {
+        vendor: 'Terra Marre',
+        title: 'Our restaurant gives you 15% discount',
+        category: 'Food',
+        tags: ['Pizza', 'Pasta', 'Focaccia' ],
+        description: 'Discount available 24/7 for all our pizzas. 10% for weekdays 15% for weekends',
+        addressList: ['Belarus, Minsk, Komsomolskaya street, 3',
+          'Belarus, Minsk, Lenina street, 25',
+          'Belarus, Minsk, Sverdlova street, 1a',
+          'Belarus, Minsk, Skryganova street, 48',
+          'Belarus, Minsk, Molodezhnaya street, 11',
+          'Belarus, Minsk, Esenina street, 5'],
+        website: 'https://www.dominos.by',
+        phones: ['7717', '(029) 750-37-16', '(044) 750-37-16'],
+        workingHours: '06:00 - 23:00',
+        validity: new Date(2021, 11, 31),
+        instagram: 'https://www.instagram.com/',
+        facebook: 'https://www.facebook.com/',
+        vk: 'https://vk.com/',
+      }
+    },
+    {
+      background: '../../../assets/images/card-backgrounds/bg-vendor-orange.png',
+      discount: {
+        vendor: 'Contenintal Barbrshop',
+        title: 'Improve your look with our 20% discount',
+        category: 'Beaty',
+        tags: ['Barbershop', ],
+        description: 'Discount available 24/7 for all our pizzas. 10% for weekdays 15% for weekends',
+        addressList: ['Belarus, Minsk, Komsomolskaya street, 3',
+          'Belarus, Minsk, Lenina street, 25',
+          'Belarus, Minsk, Sverdlova street, 1a',
+          'Belarus, Minsk, Skryganova street, 48',
+          'Belarus, Minsk, Molodezhnaya street, 11',
+          'Belarus, Minsk, Esenina street, 5'],
+        website: 'https://www.dominos.by',
+        phones: ['7717', '(029) 750-37-16', '(044) 750-37-16'],
+        workingHours: '06:00 - 23:00',
+        validity: new Date(2021, 11, 31),
+        instagram: 'https://www.instagram.com/',
+        facebook: 'https://www.facebook.com/',
+        vk: 'https://vk.com/',
+      }
+    },
+  ];
+
   constructor(public dialog: MatDialog) {}
 
-  openDiscountDetails(): void {
-    const dialogRef = this.dialog.open(DiscountDetailsModalComponent);
+  openDiscountDetails(discount: DiscountCard): void {
+    const dialogRef = this.dialog.open(DiscountDetailsModalComponent, {data: discount});
 
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
