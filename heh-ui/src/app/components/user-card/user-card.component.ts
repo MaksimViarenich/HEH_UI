2e4b242cb6875fecd6b9b7dd1771870cd59e6f98
IvanKryvenko <ivank@DESKTOP-IODRHRH>
2021-01-27 12:09:47 +0200

    HEH-UI-25: Create user card on admin tab

--- heh-ui/src/app/components/user-card/user-card.component.ts
+++ heh-ui/src/app/components/user-card/user-card.component.ts
@@ -1,3 +1,4 @@
+import { UserInfo } from './../../models/user-info';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-card.component.scss']
 })
 export class UserCardComponent implements OnInit {
+
+  user: UserInfo = {
+    name: 'Michael Browk',
+    email: 'michael.browk@exadel.com',
+    role: 'user',
+    isAtive: true,
+    avatar: '../../assets/img/header_profile.svg'
+  };
 
   constructor() { }
 
---
+++ heh-ui/src/app/models/user-info.ts
@@ -0,0 +1,7 @@
+export interface UserInfo {
+  name: string;
+  email: string;
+  role: string;
+  isAtive: boolean;
+  avatar: string;
+}
