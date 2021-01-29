6c0fc5a1dab16006767ce56df18f9bfc06be2148
Natalia Shulzhenko <savostyuk@gmail.com>
2021-01-29 13:10:01 +0300

    HEH-UI-33: component date-search was created

--- heh-ui/src/app/components/search/select/select.component.ts
+++ heh-ui/src/app/components/search/select/select.component.ts
@@ -11,6 +11,7 @@ import { SelectOption } from 'src/app/models/select-option';
 export class SelectComponent {
   @Input() options: SelectOption[];
   @Input() label: string;
+  @Input() type?: string;
 
   selectedOption = '';
 
