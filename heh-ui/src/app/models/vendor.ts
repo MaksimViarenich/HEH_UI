bd8c81582c46c8e3d1665e417d9966f6d2112dfa
IvanKryvenko <ivank@DESKTOP-IODRHRH>
2021-02-08 12:45:58 +0200

    HEH-UI-74: Add all vendors request

--- heh-ui/src/app/models/vendor.ts
+++ heh-ui/src/app/models/vendor.ts
@@ -1,17 +1,4 @@
-import { Address } from './address';
-import { Phones } from './phones';
-import {Discount} from './discount';
-
 export interface Vendor {
-    name: string;
-    addressList: Array<Address>;
-    website?: string;
-    phones: Array<Phones>;
-    workingHours: string;
-    instagram?: string;
-    facebook?: string;
-    vk?: string;
-    discounts?: Array<Discount>;
-    isReceiveNotificationsAllowed: boolean;
-    views?: number;
-  }
+  id: string;
+  name: string;
+}
---
+++ heh-ui/src/app/pages/moderator/vendors/vendor.service.ts
@@ -0,0 +1,24 @@
+import { BASE_API_URL } from './../../../global';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VendorService {
+
+  constructor(public http: HttpClient) {}
+
+  getVendors(): Observable<any> {
+
+    const token = localStorage.getItem('isAuth');
+
+    let headers = new HttpHeaders();
+
+    headers = headers.append('accept', 'application/json;odata.metadata=minimal;odata.streaming=true');
+    headers = headers.append('Authorization', `Bearer ${token}`);
+
+    return this.http.get(`${BASE_API_URL}/api/Vendor`, {headers});
+  }
+}
