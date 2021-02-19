81bb3e65c8c3d754b41807000669921260c4a8bd
Natalia Shulzhenko <savostyuk@gmail.com>
2021-02-20 00:34:50 +0300

    HEH-UI-125: change color function was added

--- heh-ui/src/app/components/header/select-background/select-background.component.ts
+++ heh-ui/src/app/components/header/select-background/select-background.component.ts
@@ -18,20 +18,18 @@ export class SelectBackgroundComponent implements OnInit {
   ngOnInit(): void {
     this.activeBackground = JSON.parse(localStorage.getItem('background') as string) || this.backgrounds[0];
     document.body.style.background = (this.activeBackground?.background as string);
-    if (this.activeBackground?.isDark) {
-      document.body.classList.remove('light');
-      document.body.classList.add('dark');
-    } else {
-      document.body.classList.remove('dark');
-      document.body.classList.add('light');
-    }
+    this.changeColorTheme(this.activeBackground);
   }
 
   changeBackground(background: Background): void {
     this.activeBackground = background;
     localStorage.setItem('background', JSON.stringify(this.activeBackground));
     document.body.style.background = background.background;
-    if (background.isDark) {
+    this.changeColorTheme(background);
+  }
+
+  changeColorTheme(background: Background | undefined): void {
+    if (background?.isDark) {
       document.body.classList.remove('light');
       document.body.classList.add('dark');
     } else {
