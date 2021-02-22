2617258ffca3e194bff39799e6c96797b92a6c04
Natalia Shulzhenko <savostyuk@gmail.com>
2021-02-22 14:02:45 +0300

    HEH-UI-125: main colors for all themes were changed

--- heh-ui/src/app/components/header/select-background/select-background.component.ts
+++ heh-ui/src/app/components/header/select-background/select-background.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Background, SelectBackgroundService } from './select-background.service';
+import { SelectBackgroundService } from './select-background.service';
 
 @Component({
   selector: 'app-select-background',
@@ -8,33 +8,71 @@ import { Background, SelectBackgroundService } from './select-background.service
 })
 export class SelectBackgroundComponent implements OnInit {
 
-  backgrounds: Array<Background>;
-  activeBackground: Background;
+  backgrounds: Array<string>;
+  activeBackground: string | undefined;
 
   constructor(private selectBackgroundServer: SelectBackgroundService) {
     this.backgrounds = selectBackgroundServer.getBackgrounds();
-    this.activeBackground = JSON.parse(localStorage.getItem('background') as string) || this.backgrounds[0];
   }
 
   ngOnInit(): void {
-    document.body.style.background = (this.activeBackground?.background as string);
+    this.activeBackground = localStorage.getItem('background') || this.backgrounds[0];
+    document.body.style.background = this.activeBackground;
     this.changeColorTheme(this.activeBackground);
   }
 
-  changeBackground(background: Background): void {
+  changeBackground(background: string): void {
     this.activeBackground = background;
-    localStorage.setItem('background', JSON.stringify(this.activeBackground));
-    document.body.style.background = background.background;
+    localStorage.setItem('background', background);
+    document.body.style.background = background;
     this.changeColorTheme(background);
   }
 
-  changeColorTheme(background: Background | undefined): void {
-    if (background?.isDark) {
-      document.body.classList.remove('light');
-      document.body.classList.add('dark');
-    } else {
-      document.body.classList.remove('dark');
-      document.body.classList.add('light');
+  changeColorTheme(background: string | undefined): void {
+    switch (background) {
+      case 'linear-gradient(90deg, #b9deed, #efefef)':
+        document.body.classList.remove('pink-yellow', 'green-blue', 'purple-blue', 'green-orange', 'dark-blue', 'dark-violet', 'dark-grey', 'dark-green', 'dark-pink');
+        document.body.classList.add('light-blue');
+        break;
+      case 'linear-gradient(90deg, rgb(245, 152, 168, 70%), rgb(246, 237, 178, 70%))':
+        document.body.classList.remove('light-blue', 'green-blue', 'purple-blue', 'green-orange', 'dark-blue', 'dark-violet', 'dark-grey', 'dark-green', 'dark-pink');
+        document.body.classList.add('pink-yellow');
+        break;
+      case 'linear-gradient(90deg, rgb(207, 236, 208, 60%), rgb(160, 206, 167, 60%), rgb(158, 192, 219, 60%))':
+        document.body.classList.remove('light-blue', 'pink-yellow', 'purple-blue', 'green-orange', 'dark-blue', 'dark-violet', 'dark-grey', 'dark-green', 'dark-pink');
+        document.body.classList.add('green-blue');
+        break;
+      case 'linear-gradient(90deg, rgb(174, 164, 227, 60%), rgb(211, 255, 232, 60%))':
+        document.body.classList.remove('light-blue', 'pink-yellow', 'green-blue', 'green-orange', 'dark-blue', 'dark-violet', 'dark-grey', 'dark-green', 'dark-pink');
+        document.body.classList.add('purple-blue');
+        break;
+      case 'linear-gradient(limegreen, transparent), linear-gradient(90deg, skyblue, transparent), linear-gradient(-90deg, coral, transparent)':
+        document.body.classList.remove('light-blue', 'pink-yellow', 'green-blue', 'purple-blue', 'dark-blue', 'dark-violet', 'dark-grey', 'dark-green', 'dark-pink');
+        document.body.classList.add('green-orange');
+        break;
+      case 'linear-gradient(315deg, #04619f 0%, #000000 74%)':
+        document.body.classList.remove('light-blue', 'pink-yellow', 'green-blue', 'purple-blue', 'green-orange', 'dark-violet', 'dark-grey', 'dark-green', 'dark-pink');
+        document.body.classList.add('dark-blue');
+        break;
+      case 'linear-gradient(315deg, #923CB5 0%, #000000 74%)':
+        document.body.classList.remove('light-blue', 'pink-yellow', 'green-blue', 'purple-blue', 'green-orange', 'dark-blue', 'dark-grey', 'dark-green', 'dark-pink');
+        document.body.classList.add('dark-violet');
+        break;
+      case 'linear-gradient(315deg, #485461 0%, #28313b 74%)':
+        document.body.classList.remove('light-blue', 'pink-yellow', 'green-blue', 'purple-blue', 'green-orange', 'dark-blue', 'dark-violet', 'dark-green', 'dark-pink');
+        document.body.classList.add('dark-grey');
+        break;
+      case 'linear-gradient(315deg, #166d3b 0%, #000000 74%)':
+        document.body.classList.remove('light-blue', 'pink-yellow', 'green-blue', 'purple-blue', 'green-orange', 'dark-blue', 'dark-violet', 'dark-grey', 'dark-pink');
+        document.body.classList.add('dark-green');
+        break;
+      case 'linear-gradient(315deg, #e84393 0%, #000000 74%)':
+        document.body.classList.remove('light-blue', 'pink-yellow', 'green-blue', 'purple-blue', 'green-orange', 'dark-blue', 'dark-violet', 'dark-grey', 'dark-green');
+        document.body.classList.add('dark-pink');
+        break;
+      default:
+        document.body.classList.remove('pink-yellow', 'green-blue', 'purple-blue', 'green-orange', 'dark-blue', 'dark-violet', 'dark-grey', 'dark-green', 'dark-pink');
+        document.body.classList.add('light-blue');
     }
   }
 }
--- heh-ui/src/app/components/header/select-background/select-background.service.ts
+++ heh-ui/src/app/components/header/select-background/select-background.service.ts
@@ -1,34 +1,27 @@
 import { Injectable } from '@angular/core';
 
-export interface Background {
-  background: string;
-  isDark?: boolean;
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class SelectBackgroundService {
-  backgrounds: Array<Background>;
+  backgrounds: Array<string>;
 
   constructor() {
     this.backgrounds = [
-      { background: 'linear-gradient(90deg, #f598a8, #f6edb2)'},
-      { background: 'linear-gradient(90deg, #cfecd0, #a0cea7, #9ec0db)'},
-      { background: 'linear-gradient(90deg, #cfecd0, #ffc5ca)'},
-      { background: 'linear-gradient(90deg, #b9deed, #efefef)'},
-      { background: 'linear-gradient(90deg, #aea4e3, #d3ffe8)'},
-      { background: 'linear-gradient(90deg, #69b7eb, #b3dbd3, #f4d6db)'},
-      { background: 'linear-gradient(limegreen, transparent), linear-gradient(90deg, skyblue, transparent), linear-gradient(-90deg, coral, transparent)'},
-      { background: 'linear-gradient(rgba(135, 60, 255, 0.4), rgba(135, 60, 255, 0.0) 80%), linear-gradient(-45deg, rgba(120, 155, 255, 0.9) 25%, rgba(255, 160, 65, 0.9) 75%)'},
-      { background: 'linear-gradient(147deg, #000000 0%, #04619f 74%)', isDark: true},
-      { background: 'linear-gradient(147deg, #000000 0%, #923CB5 74%)', isDark: true},
-      { background: 'linear-gradient(315deg, #2234ae 0%, #191714 74%)', isDark: true},
-      { background: 'linear-gradient(315deg, #485461 0%, #28313b 74%)', isDark: true},
+      'linear-gradient(90deg, #b9deed, #efefef)',
+      'linear-gradient(90deg, rgb(245, 152, 168, 70%), rgb(246, 237, 178, 70%))',
+      'linear-gradient(90deg, rgb(207, 236, 208, 60%), rgb(160, 206, 167, 60%), rgb(158, 192, 219, 60%))',
+      'linear-gradient(90deg, rgb(174, 164, 227, 60%), rgb(211, 255, 232, 60%))',
+      'linear-gradient(limegreen, transparent), linear-gradient(90deg, skyblue, transparent), linear-gradient(-90deg, coral, transparent)',
+      'linear-gradient(315deg, #04619f 0%, #000000 74%)',
+      'linear-gradient(315deg, #923CB5 0%, #000000 74%)',
+      'linear-gradient(315deg, #485461 0%, #28313b 74%)',
+      'linear-gradient(315deg, #166d3b 0%, #000000 74%)',
+      'linear-gradient(315deg, #e84393 0%, #000000 74%)',
     ];
   }
 
-  getBackgrounds(): Array<Background> {
+  getBackgrounds(): Array<string> {
     return this.backgrounds;
   }
 }
