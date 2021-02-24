1c54f765cee433fdb07842fe14671d5252e053a6
andrski <andrski2009@gmail.com>
2021-02-24 14:59:41 +0300

    HEH-HI-157: add notifications count

---
+++ heh-ui/src/app/components/header/header.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BASE_API_URL } from 'src/app/global';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+
+  constructor(private http: HttpClient) {}
+
+  getNotificationsCount(): Observable<any> {
+    const token = localStorage.getItem('isAuth');
+    let headers = new HttpHeaders();
+    headers = headers.append('Authorization', `Bearer ${token}`);
+    headers = headers.append('accept', 'application/json;odata.metadata=minimal;odata.streaming=true');
+
+    return this.http.get(`${BASE_API_URL}/api/notification/count`, {headers});
+  }
+}
