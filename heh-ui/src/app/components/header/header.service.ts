b407422752634a77119a41d9d94b94dd08b82f9a
andrski <andrski2009@gmail.com>
2021-02-25 12:42:10 +0300

    HEH-HI-157: prepare for intercept

--- heh-ui/src/app/components/header/header.service.ts
+++ heh-ui/src/app/components/header/header.service.ts
@@ -12,8 +12,7 @@ export class HeaderService {
 
   getNotificationsCount(): Observable<any> {
     const token = localStorage.getItem('isAuth');
-    let headers = new HttpHeaders();
-    headers = headers.append('Authorization', `Bearer ${token}`);
+    
     headers = headers.append('accept', 'application/json;odata.metadata=minimal;odata.streaming=true');
 
     return this.http.get(`${BASE_API_URL}/api/notification/count`, {headers});
