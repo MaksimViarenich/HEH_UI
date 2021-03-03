73861e28ec6b165a2e74edf74643acc6d0355c3f
IvanKryvenko <ivank@DESKTOP-IODRHRH>
2021-03-03 19:50:52 +0200

    HEH-UI-210: Make location search field a tree

--- heh-ui/src/app/components/page-search/page-search.component.ts
+++ heh-ui/src/app/components/page-search/page-search.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, OnInit, ViewEncapsulation, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation, Output, EventEmitter, ViewChild } from '@angular/core';
 import { FiltersService } from '../../services/filter-service/filters.service';
 import { FormControl } from '@angular/forms';
-import { isEqual, slice, size, toString, indexOf } from 'lodash';
+import { isEqual, slice, size, toString, indexOf, forEach, includes, map, find } from 'lodash';
 
 @Component({
   selector: 'app-page-search',
@@ -19,6 +19,10 @@ export class PageSearchComponent implements OnInit {
   pickerDate: any[];
   maxDate: Date;
   month = '';
+  locations: any;
+  locationsArrayForOptions: any;
+  viewLocation: any;
+  currentLocation: any;
 
   categoriesFormControl = new FormControl();
   tagsFormControl = new FormControl();
@@ -45,10 +49,63 @@ export class PageSearchComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     this.searchData.location = sessionStorage.getItem('location');
+    this.currentLocation = sessionStorage.getItem('location');
     this.submitSearch();
     this.filtersService.loadFilters().then(() => {
       this.filtersOptions = this.filtersService.getFilters();
+      this.locations = this.destructurizeLocations(this.filtersOptions.locations);
+      this.locationsArrayForOptions = this.fillLocationOptionArray(this.locations);
     });
+  }
+
+  destructurizeLocations(locations: any): any {
+    const destructurizeArray: { country: any, cities: any }[] = [];
+    const countries: Array<string>[] = [];
+    forEach(locations, (location) => {
+      if (!includes(countries, location.viewValue.split(', ')[0])) {
+        countries.push(location.viewValue.split(', ')[0]);
+        destructurizeArray.push({
+          country: location.viewValue.split(', ')[0],
+          cities: this.getCities(location.viewValue.split(', ')[0], locations)
+        });
+      }
+    });
+
+    return destructurizeArray;
+  }
+
+  isString(value: any): any {
+    return typeof value === 'string';
+  }
+
+  fillLocationOptionArray(locations: any): any {
+    let array: any = [];
+    forEach(locations, (location) => {
+      array = isEqual(size(array), 0) ? [location.country, ...location.cities] : [...array, location.country, ...location.cities];
+    });
+
+    return array;
+  }
+
+
+  getCities(country: string, locations: any): any {
+    const cities: { id: any; city: any; }[] = [];
+    forEach(locations, (location) => {
+      if (isEqual(location.viewValue.split(', ')[0], country)) {
+        cities.push({
+          id: location.id,
+          city: location.viewValue.split(', ')[1]
+        });
+      }
+    });
+
+    return cities;
+  }
+
+  allCitiesAddresses(country: string): any {
+    if (country) {
+      this.searchData.location = map(find(this.locations, location => location.country === country).cities, city => city.id);
+    }
   }
 
   transformPickerDate(objDate: any): string {
@@ -74,6 +131,10 @@ export class PageSearchComponent implements OnInit {
   }
 
   submitSearch(): void {
+    if (!(size(this.currentLocation) < 30)) {
+      this.searchData.location = this.currentLocation;
+    }
+    console.log(this.searchData.location);
     this.applySearch.emit(this.searchData);
     this.pickerDate = [];
   }
